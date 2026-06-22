5df5e8c2bd4dd6de0b8f8865270ae80eb4415ff4
intel <MOLI>
2026-06-22 23:02:40 +0330

    menuClass

---
+++ MENU.java
@@ -0,0 +1,25 @@
+class MENU {
+    static void showStudentMenu() {
+        System.out.println("\n=====STUDENT =====");
+        System.out.println("1. LIBRARY");
+        System.out.println("2. ENTER");
+        System.out.println("3. SEARCH");
+        System.out.println("4. EXIT");
+        System.out.print("YOUR CHOICE: ");
+    }
+
+    static void showLibrarianMenu() {
+        System.out.println("\n===== LIBRARIAN =====");
+        System.out.println("1. ENTER");
+        System.out.println("2. EDIT");
+        System.out.println("3. ADD");
+        System.out.println("4. EXIT");
+        System.out.print("YOUR CHOICE: ");
+    }
+    static void showManagerMenu() {
+        System.out.println("\n===== MANAGER =====");
+        System.out.println("1. ADD");
+        System.out.println("2. EXIT");
+        System.out.print("YOUR CHOICE: ");
+    }
+}
